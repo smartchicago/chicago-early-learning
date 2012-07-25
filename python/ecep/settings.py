d559740f7d7064981fdc627c89d63044b24df8a1
David Zwarg <dzwarg@azavea.com>
2012-07-25 15:16:02 -0700

    Added 'portal' app.
    The 'portal' app contains the models, tests, and views for the ECEP portal.
    The data model is reproduced in models.py, based off google doc data model
    from smartchicago.

--- python/ecep/settings.py
+++ python/ecep/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ecep.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'django.contrib.gis',
+    'ecep.portal',
 )
 
 # A sample logging configuration. The only tangible logging
