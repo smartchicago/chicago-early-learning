3a0eaff4f920658c05fcf4c5fdb629e0777637e0
David Zwarg <dzwarg@azavea.com>
2012-07-26 09:47:26 -0700

    Updated settings.py
    Changed the name of the dependent app to 'portal' instead of 'ecep.portal'.
    Apparently, gunicorn doesn't like it, even though it seems to work in the
    development server.

--- python/ecep/settings.py
+++ python/ecep/settings.py
@@ -118,7 +118,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'django.contrib.gis',
-    'ecep.portal',
+    'portal',
 )
 
 # A sample logging configuration. The only tangible logging
