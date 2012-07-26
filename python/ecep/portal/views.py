6c43b85df366ece828fd650abbf7645693f5319f
Bennet Huber <bhuber@azavea.com>
2012-07-26 18:48:44 -0400

    Got initial template loading

--- python/ecep/portal/views.py
+++ python/ecep/portal/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+
+from django.template import Context, loader
+from django.http import HttpResponse
+
+def index(request):
+   t = loader.get_template('index.html')
+   c = Context({ })
+   return HttpResponse(t.render(c))
+
+
