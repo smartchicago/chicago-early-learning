acde66dfe508041135a03bfcacf3e8cf46f0ac4e
David Zwarg <dzwarg@azavea.com>
2013-01-10 10:43:32 -0500

    Removed obsolete views.py file for project.

--- python/ecep/views.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2012 Azavea, Inc.
-# See LICENSE in the project root for copying permission
-
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello, world.2")
