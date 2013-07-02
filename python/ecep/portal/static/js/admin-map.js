27a15ee99941589e8437c98068fb4d0ecc141abe
Chris Brown <cbrown@lagos.internal.azavea.com>
2013-07-02 17:49:25 -0400

    Add list of dependencies to header

--- python/ecep/portal/static/js/admin-map.js
+++ python/ecep/portal/static/js/admin-map.js
@@ -1,6 +1,7 @@
 /********************************************************
  * Copyright (c) 2013 Azavea, Inc.
  * See LICENSE in the project root for copying permission
+ * Requires: google-maps-api-v3; jquery-1.7.2
  ********************************************************/
 
 var ecepAdmin = ((typeof ecepAdmin === 'undefined') ? {} : ecepAdmin);
