cb03ef5e2e957f02f8d06edc08a08511d11f65f3
Chris Brown <cbrown@lagos.internal.azavea.com>
2013-07-02 13:58:49 -0400

    Remove unused code

--- python/ecep/portal/widgets.py
+++ python/ecep/portal/widgets.py
@@ -4,7 +4,6 @@ from django.utils.safestring import mark_safe
 
 class MapWidget(widgets.HiddenInput):
     def render(self, name, value, attrs=None):
-        #import ipdb; ipdb.set_trace()
         widget = super(MapWidget, self).render(name, value, attrs)
         return mark_safe("""<input name="geom" readonly="readonly" value="%s" type="text" id="id_geom" size="60">
                          <br>
