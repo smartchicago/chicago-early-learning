308cdde705432aa2b259c881e9d22da28d27e405
Bennet Huber <bhuber@azavea.com>
2013-07-11 14:20:04 -0400

    Move over relevant views from old version

--- python/ecep/portal/views.py
+++ python/ecep/portal/views.py
@@ -17,8 +17,83 @@ from django.utils import translation
 logger = logging.getLogger(__name__)
 
 
+# TODO: We probably don't need this function for the new version, I'm moving
+# it in from the old version for now to avoid lots of refactoring.
+def get_opts(selected_val='2'):
+    """
+    Gets option list for the distance dropdown (see base.html)
+    selected_val: string representing the value of the dropdown that should be selected
+    Default is '2'
+    """
+    # Options for distance dropdown
+    # option value => (option text, enabled)
+    distance_opts = { '-1': [_('Distance'), False],
+                      '0.5': [_('< 0.5 mi'), False],
+                      '1': [_('< 1 mi'), False],
+                      '2': [_('< 2 mi'), False],
+                      '5': [_('< 5 mi'), False],
+                      '10': [_('< 10 mi'), False] }
+
+    key = selected_val if selected_val in distance_opts else '2'
+    distance_opts[key][1] = True
+    result = [[k] + v for k, v in distance_opts.items()]
+    return sorted(result, key=lambda a: float(a[0]))
+
+
 def index(request):
     ctx = RequestContext(request, { })
     response = render_to_response('index.html', context_instance=ctx)
     return response
 
+
+def about(request):
+    ctx = RequestContext(request, {
+        'options': get_opts(),
+        'mapbarEnabled': True
+    })
+    return render_to_response('about.html', context_instance=ctx)
+
+
+class TopicWrapper(object):
+    topic = None
+    questions = None
+
+    def __init__(self, t, request):
+        self.topic = t
+        qs = Question.objects.filter(topic=t, status=Question.ACTIVE)
+        if request.user.is_anonymous():
+            qs = qs.exclude(protected=True)
+        self.questions = list(qs)
+
+
+def faq(request):
+    tpl = 'faq-models.html'
+
+    # get the language of the request
+    lang = request.LANGUAGE_CODE
+
+    # get the topics in this language
+    topics = Topic.objects.filter(slug__startswith=lang + '-')
+    if topics.count() == 0:
+        topics = Topic.objects.filter(slug__startswith=settings.LANGUAGE_CODE[0:2] + '-')
+        if topics.count() == 0:
+            raise Http404()
+
+    ctx = RequestContext(request, {
+        'topics': [TopicWrapper(t, request) for t in topics],
+        'options': get_opts(),
+        'mapbarEnabled': True
+    })
+    return render_to_response(tpl, context_instance=ctx)
+
+
+def setlang(request, language):
+    nxt = '/'
+    if 'next' in request.REQUEST:
+        nxt = request.REQUEST['next']
+
+    response = HttpResponseRedirect(nxt)
+    if language and check_for_language(language):
+        response.set_cookie(settings.LANGUAGE_COOKIE_NAME, language)
+
+    return response
--- python/ecep/urls.py
+++ python/ecep/urls.py
@@ -18,8 +18,34 @@ urlpatterns = patterns(
     '',
     # Index page is in the 'portal' app
     url(r'^$', 'portal.views.index'),
+    url(r'^about.html$', 'portal.views.about', name='about'),
+    url(r'^robots\.txt$', direct_to_template,
+        {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to',
+        {'url': '/static/images/favicon.ico'}),
+
+    # Telephony
+    url(r'^sms/handler/?$', Sms.as_view()),
+    url(r'^sms/error/?$', 'django_twilio.views.sms', {
+        'message': Conversation.FATAL,
+        'method': 'POST',
+        # Due to a bug in django-twilio, method must be set to GET or POST
+        # it works no matter what the request is
+        }),
+    url(r'^sms/callback/?$', SmsCallback.as_view(), name='sms-callback'),
+
+    # i18n
+    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
+    url(r'^rosetta/', include('rosetta.urls')),
+    url(r'^setlang/(?P<language>.+)/$', 'portal.views.setlang', name='setlang'),
+
 
     # Admin interface
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += i18n_patterns(
+    '',
+    url(r'^faq.html$', 'portal.views.faq', name='faq'),
 )
 
