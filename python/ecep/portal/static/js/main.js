b4df184f8a6f8412067e67235929de2f20450c5e
Bennet Huber <bhuber@azavea.com>
2013-07-15 16:45:06 -0400

    Get require modules loading

--- python/ecep/portal/static/js/main.js
+++ python/ecep/portal/static/js/main.js
@@ -1,2 +1,26 @@
-/* Dummy JS file, will replace later */
+/* Main app file for CEL project
+ * Mostly stolen from requirejs examples: http://requirejs.org/docs/api.html
+ */
 
+'use strict';
+ 
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: (window && window.cel_baseUrl) || 'static/js/lib',
+    //except, if the module ID starts with 'app',
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a '.js' extension since
+    //the paths config could be for a directory.
+    paths: {
+        cel: '../cel',
+        jquery: 'jquery-1.10.2.min'
+    }
+});
+
+require(['jquery'], function($) {
+    console.debug('main.js loaded!');
+    if (typeof $ !== 'undefined') {
+        console.debug('jquery loaded!');
+    }
+});
