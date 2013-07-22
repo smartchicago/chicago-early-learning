003f28fbe26377ad796569566e1273633443ea99
Chris Brown <cbrown@azavea.com>
2013-07-22 16:43:31 -0400

    Refactor function to just a return

--- python/ecep/portal/static/js/lib/icons.js
+++ python/ecep/portal/static/js/lib/icons.js
@@ -8,26 +8,27 @@
 
 define(['./Leaflet'], function(L) {
     // Return Icons for Leaflet maps
-    var schoolIcon = L.icon({
-        iconUrl: '/static/img/leaflet-icons/marker-school.png',
-        shadowUrl: '/static/img/leaflet-icons/marker-shadow.png',
+    return {
+        schoolIcon: L.icon({
+            iconUrl: '/static/img/leaflet-icons/marker-school.png',
+            shadowUrl: '/static/img/leaflet-icons/marker-shadow.png',
 
-        iconSize:     [35, 45], // size of the icon
-        shadowSize:   [41, 41], // size of the shadow
-        iconAnchor:   [10, 60], // point of the icon which will correspond to marker's location
-        shadowAnchor: [4, 62],  // the same for the shadow
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-    var homeIcon = L.icon({
-        iconUrl: '/static/img/leaflet-icons/marker-home.png',
-        shadowUrl: '/static/img/leaflet-icons/marker-shadow.png',
-
-        iconSize:     [35, 45], // size of the icon
-        shadowSize:   [41, 41], // size of the shadow
-        iconAnchor:   [10, 60], // point of the icon which will correspond to marker's location
-        shadowAnchor: [4, 62],  // the same for the shadow
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-    return {'schoolIcon': schoolIcon, 'homeIcon': homeIcon};
+            iconSize:     [35, 45], // size of the icon
+            shadowSize:   [41, 41], // size of the shadow
+            iconAnchor:   [10, 60], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        }),
+        homeIcon: L.icon({
+            iconUrl: '/static/img/leaflet-icons/marker-home.png',
+            shadowUrl: '/static/img/leaflet-icons/marker-shadow.png',
+            
+            iconSize:     [35, 45], // size of the icon
+            shadowSize:   [41, 41], // size of the shadow
+            iconAnchor:   [10, 60], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        })
+    };
 });
 
