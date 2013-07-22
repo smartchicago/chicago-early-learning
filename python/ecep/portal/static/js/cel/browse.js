f8179a4ea420fa1f4ffa931ba1291809a1057bb4
Chris Brown <cbrown@azavea.com>
2013-07-22 16:01:55 -0400

    Formatting

--- python/ecep/portal/static/js/cel/browse.js
+++ python/ecep/portal/static/js/cel/browse.js
@@ -13,66 +13,66 @@ define(['jquery', 'Leaflet', 'text!templates/neighborhoodList.html', 'text!templ
             locations,    // Store location data
             neighborhoods,    // Store neighborhood data
             template,    // Hold handlebars template
-	    listView = 'none',    // Keep track of current result list type
-	    locationLayer = new L.LayerGroup(),   // Location/school layer group
-	    neighborhoodLayer = new L.LayerGroup(),   // Neighborhood layer group
-	    neighborhoodGeojson;    // Store neighborhood geojson
+            listView = 'none',    // Keep track of current result list type
+            locationLayer = new L.LayerGroup(),   // Location/school layer group
+            neighborhoodLayer = new L.LayerGroup(),   // Neighborhood layer group
+            neighborhoodGeojson;    // Store neighborhood geojson
 
         /**
          * Loads Json data for neighborhoods and locations
          * then displays one based on current zoom level
          */
-	var loadData = function() {
-	    $.when(
-		$.getJSON(window.location.origin + '/api/location/', function(data) {
+        var loadData = function() {
+            $.when(
+                $.getJSON(window.location.origin + '/api/location/', function(data) {
                     locations = data.locations;
-		}),
+                }),
                 $.getJSON('/api/neighborhood/', function(data) {
                     neighborhoods = data;
                 }),
-		$.getJSON(window.location.origin + '/static/js/neighborhoods-topo.json', function(data) {
-		    neighborhoodLayer = L.geoJson(null, {
-			style: {
-			    color: '#666',
-			    weight: 1,
-			    opacity: 1,
-			    fillOpacity: 0.3
-			}
-		    });
-		    neighborhoodGeojson = topojson.feature(data, data.objects.neighborhoods);
-		})).then(displayMap);
-	};
+                $.getJSON(window.location.origin + '/static/js/neighborhoods-topo.json', function(data) {
+                    neighborhoodLayer = L.geoJson(null, {
+                        style: {
+                            color: '#666',
+                            weight: 1,
+                            opacity: 1,
+                            fillOpacity: 0.3
+                        }
+                    });
+                    neighborhoodGeojson = topojson.feature(data, data.objects.neighborhoods);
+                })).then(displayMap);
+        };
 
         /**
          * Controls logic of whether to display locations or neighborhoods
          * based on current zoom level. Called when map is initialized
          * and after a change in zoom level.
          */
-	var displayMap = function() {
-	    var zoom_level = map.getZoom();
-	    if (listView !== 'neighborhoods' && zoom_level < 13) {
+        var displayMap = function() {
+            var zoom_level = map.getZoom();
+            if (listView !== 'neighborhoods' && zoom_level < 13) {
                 // If not already displaying neighborhoods and zoomed out
                 listView = 'neighborhoods';
                 listResults(neighborhoods);
-		locationLayer.clearLayers();
-		neighborhoodLayer.clearLayers();
-		neighborhoodLayer.addData(neighborhoodGeojson);
-		map.addLayer(neighborhoodLayer);
-	    }
-	    else if (listView !== 'locations' && zoom_level >= 13) {
+                locationLayer.clearLayers();
+                neighborhoodLayer.clearLayers();
+                neighborhoodLayer.addData(neighborhoodGeojson);
+                map.addLayer(neighborhoodLayer);
+            }
+            else if (listView !== 'locations' && zoom_level >= 13) {
                 // If not already displaying locations and zoomed in
-		map.removeLayer(neighborhoodLayer);
-		map.addLayer(locationLayer);
+                map.removeLayer(neighborhoodLayer);
+                map.addLayer(locationLayer);
                 listView = 'locations';
                 listResults(locations);
-		for( var i=0; i < locations.length; i++){
-		    var lat = locations[i].position.lat,
-			lng = locations[i].position.lng,
-			loc_marker = L.marker([lat, lng], {icon: icons.schoolIcon});
-		    locationLayer.addLayer(loc_marker);
-		};
-	    }
-	};
+                for( var i=0; i < locations.length; i++){
+                    var lat = locations[i].position.lat,
+                        lng = locations[i].position.lng,
+                        loc_marker = L.marker([lat, lng], {icon: icons.schoolIcon});
+                    locationLayer.addLayer(loc_marker);
+                };
+            }
+        };
 
         /**
          * Changes list results display using Handlebars templates
