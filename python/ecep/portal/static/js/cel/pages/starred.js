dd3d77d192dce3c4e6086e2f3fa931e99afcb3ec
CloudNiner <afink@azavea.com>
2013-07-23 13:33:31 -0400

    Move cookie settings to js module

--- python/ecep/portal/static/js/cel/pages/starred.js
+++ python/ecep/portal/static/js/cel/pages/starred.js
@@ -6,10 +6,12 @@
 
 'use strict';
  
-define(['jquery', 'Leaflet', 'text!templates/location.html', 'common', 'jquery-cookie'], 
-    function($, L, html, common) {
+define(['jquery', 'Leaflet', 'text!templates/location.html', 'common', 'celcookie', 'jquery-cookie'], 
+    function($, L, html, common, celcookie) {
 
         $(document).ready(function() {
+
+            console.log('CEL COOKIES: ', celcookie);
     
             // Draw the Handlebars template for a location 
             function drawStarredLocations(data) {
@@ -25,23 +27,27 @@ define(['jquery', 'Leaflet', 'text!templates/location.html', 'common', 'jquery-c
                 $('#fav-count').html(data.locations.length);
             }
 
-            var cookieopts = CEL.serverVars.starredcookie;
-
             // get location ids:
             // url --> string :: /starred/12,13,54/  --> "12,13,54" 
             //      -- or --
             // cookie string 
-            var cookie = $.cookie(cookieopts.name),
+            var cookie = $.cookie(celcookie.name),
                 regexResult = /([0-9,]+)/.exec(window.location.pathname),
-                starredIds = regexResult[1] || cookie || "";
+                starredIds = "";
+            
+            if (regexResult || cookie) {
+                starredIds = regexResult ? regexResult[1] : cookie;
+                $.getJSON(common.getUrl('location-api') + starredIds, drawStarredLocations);
+            } else {
+                $('.container').html('No Starred Locations');
+            }
 
-            $.getJSON(common.getUrl('location-api') + starredIds, drawStarredLocations);
 
             // Click event for "Clear Selections" button
             $('#faves-clear').bind('click', function(e) {
                 $('.container').empty();
                 $('#fav-count').html(0);
-                $.removeCookie(cookieopts.name, cookieopts.options);
+                $.removeCookie(celcookie.name, celcookie.options);
             });
 
             // Click event for "Share" button
