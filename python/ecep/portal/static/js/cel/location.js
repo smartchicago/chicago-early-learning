09f3a8f203a72834be53313832a8c148e00f1033
CloudNiner <afink@azavea.com>
2013-07-26 14:27:51 -0400

    Add more functionality to location object

--- python/ecep/portal/static/js/cel/location.js
+++ python/ecep/portal/static/js/cel/location.js
@@ -1,7 +1,7 @@
-/********************************************************                                           
- * Copyright (c) 2013 Azavea, Inc.                                                                  
- * See LICENSE in the project root for copying permission                                           
- * Location with definition of location objects and 
+/********************************************************
+ * Copyright (c) 2013 Azavea, Inc.
+ * See LICENSE in the project root for copying permission
+ * Location with definition of location objects and
  * data loader object
  *********************************************************/
 
@@ -27,9 +27,9 @@ define(['jquery', 'Leaflet', 'favorites'], function($, L, favorites) {
      */
     Location.prototype.getId = function() {
         return this.data.item.key;
-    }
+    };
 
-    /* 
+    /*
      * Checks if location is starred
      * Returns:
      *      true if starred
@@ -44,43 +44,136 @@ define(['jquery', 'Leaflet', 'favorites'], function($, L, favorites) {
         } else {
             return false;
         }
-    }
+    };
 
-    /* 
+    /*
      * Add location to cookie string
      */
     Location.prototype.setStarred = function() {
         favorites.addIdToCookie(this.id);
-    }
+    };
+
+    /*
+     * Is the location accredited?
+     */
+    Location.prototype.isAccredited = function() {
+        // quick way.
+        return (this.data.sfields[0].value === "None") ? false : true;
+        // detailed way
+        /*
+        var isAccredited = false;
+        $.each(this.data.sfields, function(key, value) {
+            if (value.fieldname === "Accreditation" && value.value !== "None") {
+                isAccredited = true;
+                return false;
+            }
+        });
+        */
+    };
+
+    /*
+     * Determines if the location is a school or not
+     * Returns:
+     *      true if school
+     *      false if not
+     */
+    Location.prototype.isSchool = function() {
+        return true;
+    };
 
     /* Function that returns appropriate icon based on
      * a location's properties
      */
-    Location.prototype.getIcon = function(options){
-        // TO DO: Add in functionality to get icon based on 
-        // characteristics of location
-        var defaults = {
+    Location.prototype.getIcon = function(options) {
+
+        var setHighlighted = function(options) {
 
         };
-        var opts = $.extend({}, defaults, options);
+
+        var defaults = {
+            highlighted: false,
+            iconUrl: '/static/img/leaflet-icons/marker-icon.png',
+            shadowUrl: '/static/img/leaflet-icons/marker-shadow.png',
+            iconSize: [35, 45],
+            shadowSize: [41, 41],
+            iconAnchor: [10, 60],
+            shadowAnchor: [4, 62],
+            popupAnchor: [10, -60]
+        };
+        var iconOpts = $.extend({}, defaults, options),
+            key = '';
+
+        // build a key!
+        key += (this.isSchool()) ? 'school' : 'center';
+        key += (this.isStarred()) ? '-starred' : '';
+        key += (this.isAccredited()) ? '-accredited' : '';
+
+        // if the icon already exists in cache, just return reference to that
+        //if (DataManager.IconCache[key]) {
+            //console.log("From DataManager.IconCache:", DataManager.IconCache[key]);
+            //return DataManager.IconCache[key];
+        //}
+
+
+        switch (key) {
+            case 'school':
+                $.extend(iconOpts, {iconUrl: '/static/img/leaflet-icons/marker-school.png'});
+                break;
+            case 'school-starred':
+                $.extend(iconOpts, {iconUrl: '/static/img/leaflet-icons/marker-school.png'});
+                break;
+            case 'school-accredited':
+                $.extend(iconOpts, {iconUrl: '/static/img/leaflet-icons/marker-school.png'});
+                break;
+            case 'school-accredited-starred':
+                $.extend(iconOpts, {iconUrl: '/static/img/leaflet-icons/marker-school.png'});
+                break;
+            case 'center':
+                $.extend(iconOpts, {iconUrl: '/static/img/leaflet-icons/marker-home.png'});
+                break;
+            case 'center-starred':
+                $.extend(iconOpts, {iconUrl: '/static/img/leaflet-icons/marker-home.png'});
+                break;
+            case 'center-accredited':
+                $.extend(iconOpts, {iconUrl: '/static/img/leaflet-icons/marker-home.png'});
+                break;
+            case 'center-accredited-starred':
+                $.extend(iconOpts, {iconUrl: '/static/img/leaflet-icons/marker-home.png'});
+                break;
+            case 'geolocation':
+                break;
+            default:
+                return null;
+        }
+
+        // highlighting means we double all dimensions and replace the icon with @2x
+        if (iconOpts.highlighted) {
+            iconOpts = setHighlighted(iconOpts);
+        }
+
+        var icon = L.icon(iconOpts);
+        //DataManager.IconCache[key] = icon;
+        return icon;
     };
 
     /* Sets boolean value for if location should be on list
      */
     Location.prototype.showInList = function(filters, map){
-        // if location filter 
+        // if location filter
     };
 
     var DataLoader = {
+
+        iconcache: {},
 
         /* Updates if location is shown in map and list based on
          * applied filters and bounding box of map
          */
         locationUpdate: function(filters, map) {},
-        
+
         /* Updates school counts for neighborhoods based on
          * filters
-         * 
+         *
          * @param {Filters taken from filter-list/model} filters
          */
         neighborhoodUpdate: function(filters, neighborhoodLayer) {},
