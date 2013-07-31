42abce08e1fd3529e104a7e400bc1273aa43389c
CloudNiner <afink@azavea.com>
2013-07-31 11:00:59 -0400

    Homepage styling edits

--- python/ecep/portal/static/js/cel/common.js
+++ python/ecep/portal/static/js/cel/common.js
@@ -178,12 +178,14 @@ function($, L, Response, Handlebars) {
     });
 
     // Hide the address bar on mobile browsers
+    // TODO: More robust solution: http://mobile.tutsplus.com/tutorials/mobile-web-apps/remove-address-bar/
+    /*
     window.addEventListener("load",function() {
         setTimeout(function(){
             window.scrollTo(0, 1);
         }, 0);
     });
-			
+    */
 			
     // Tooltips for all!  Anything w/ a tooltip tag gets a tooltip
     $('[rel="tooltip"]').tooltip();
