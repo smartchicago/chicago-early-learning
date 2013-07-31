60d201b1415852514be25cf9d3c6a5b662ce7415
Bennet Huber <bhuber@azavea.com>
2013-07-30 22:36:02 -0400

    Fix popovers disappearing on pan

--- python/ecep/portal/static/js/cel/pages/browse.js
+++ python/ecep/portal/static/js/cel/pages/browse.js
@@ -94,7 +94,7 @@ define(['jquery', 'Leaflet', 'text!templates/neighborhoodList.html', 'text!templ
             var zoomLevel = map.getZoom();
 
             if (isAutocompleteSet && autocompleteLocationId) {
-                dm.locationUpdate(map);
+                dm.locationUpdate(map, locationLayer);
             } else if (isAutocompleteSet && autocompleteNeighborhoodId) {
                 dm.neighborhoodUpdate(map);
             } else if (currentLayer !== layerType.neighborhood) {
@@ -103,12 +103,12 @@ define(['jquery', 'Leaflet', 'text!templates/neighborhoodList.html', 'text!templ
                     dm.neighborhoodUpdate(map);
                 } else {
                     // We're still good, update locations
-                    dm.locationUpdate(map);
+                    dm.locationUpdate(map, locationLayer);
                 }
             } else if (currentLayer !== layerType.location) {
                 if (zoomLevel >= zoomSettings) {
                     // We zoomed in, switch to locations
-                    dm.locationUpdate(map);
+                    dm.locationUpdate(map, locationLayer);
                 } else {
                     // We're still good, update neighborhoods
                     dm.neighborhoodUpdate(map);
@@ -296,7 +296,10 @@ define(['jquery', 'Leaflet', 'text!templates/neighborhoodList.html', 'text!templ
          */
         dm.events.on("DataManager.neighborhoodUpdated", function(e) {
             // If not already displaying neighborhoods and zoomed out
-            currentLayer = layerType.neighborhood;
+            if (currentLayer !== layerType.neighborhood) {
+                currentLayer = layerType.neighborhood;
+            }
+
             listResults(dm.neighborhoods.data, currentLayer);
             locationLayer.clearLayers();
             neighborhoodLayer.clearLayers();
@@ -316,18 +319,14 @@ define(['jquery', 'Leaflet', 'text!templates/neighborhoodList.html', 'text!templ
          */
         dm.events.on("DataManager.locationUpdated", function(e) {
             // If not already displaying locations and zoomed in
-            currentLayer = layerType.location;
-            map.removeLayer(neighborhoodLayer);
-            locationLayer.clearLayers();
-            map.addLayer(locationLayer);
-            popupLayer.clearLayers();
-            listResults(dm.locations, currentLayer);
+            if (currentLayer !== layerType.location) {
+                currentLayer = layerType.location;
+                popupLayer.clearLayers();
+            }
 
-            // Create map markers and bind popups
-            $.each(dm.locations, function(i, location) {
-                var locMarker = location.getMarker();
-                locationLayer.addLayer(locMarker);
-            });
+            map.removeLayer(neighborhoodLayer);
+            map.addLayer(locationLayer);
+            listResults(dm.locations, currentLayer);
 
             // set map to location/neighborhood if autocomplete requested it
             setAutocompleteLocation();
@@ -337,7 +336,7 @@ define(['jquery', 'Leaflet', 'text!templates/neighborhoodList.html', 'text!templ
 
         // Load data and build map when page loads
         return {
-            init: function(){
+            init: function() {
                 var state = getMapState();
                 map = new L.map('map').setView(state.point, defaultZoom);   // Initialize Leaflet map
                 L.tileLayer.provider('Acetate.all').addTo(map);             // basemap
@@ -374,10 +373,12 @@ define(['jquery', 'Leaflet', 'text!templates/neighborhoodList.html', 'text!templ
                 // set up social sharing for the top button (next to favorites)
                 $('#share-favorites-btn').on('click', favorites.initShareModal);
                 
+                // Bind filtering click handlers
                 $filters.on('click', function() { dm.onFilterChange(); });
                 $filterClearAll.on('click', function() {
                     $filters.prop('checked', false);
                 });
+
                 mapToggle();
                 displayMap();
             }
