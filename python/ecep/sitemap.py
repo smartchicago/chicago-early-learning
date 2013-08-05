1bbe4ec57f5f13a9c44ec64c51c8d01c0fefed14
Chris Brown <cbrown@azavea.com>
2013-08-05 11:06:09 -0400

    Add sitemap classes for static pages and locations

---
+++ python/ecep/sitemap.py
@@ -0,0 +1,35 @@
+# Sitemap Settings
+# Note - You need to set the site in the admin
+# in order to have the correct URLs in the sitemap
+
+from django.contrib import sitemaps
+from django.core.urlresolvers import reverse
+
+from portal.models import Location
+
+class LocationSiteMap(sitemaps.Sitemap):
+    """
+    Sitemap for individual location views
+    """
+    changefreq = 'weekly'
+    priority = 0.5
+
+    def items(self):
+        return Location.objects.all()
+
+    def location(self, item):
+        return reverse('location-view', args=(item.id,))
+
+class StaticViewSitemap(sitemaps.Sitemap):
+    """
+    Sitemaps for index, about, faq, and browse pages
+    """
+    changefreq = 'weekly'
+    priority = 0.5
+
+    def items(self):
+        return ['index', 'about', 'faq', 'browse']
+
+    def location(self, item):
+        return reverse(item)
+
