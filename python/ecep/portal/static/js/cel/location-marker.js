a640cff97b1c410017d56ceaefb5abbd9ebff1c3
CloudNiner <afink@azavea.com>
2013-08-15 14:45:39 -0400

    Add schema.org rich markup to single location view

--- python/ecep/portal/static/js/cel/location-marker.js
+++ python/ecep/portal/static/js/cel/location-marker.js
@@ -4,53 +4,52 @@
  * Requires: google-maps-api-v3; jquery; leaflet;
  ********************************************************/
 
-define(['jquery', 'Leaflet', 'Handlebars', 'text!templates/location.html', 'location',
+define(['jquery', 'Leaflet', 'location',
        'common', 'favorites', CEL.serverVars.gmapRequire, 'leaflet-providers'], 
-    function($, L, Handlebars, html, location, common, favorites) {
+    function($, L, location, common, favorites) {
         'use strict';
 
         /* On page load, query api to get locations position, add marker to map
          * for location. Use google maps layer for leaflet.
          */
         $(document).ready(function() {
-            var location_id = /(\d+)/.exec(window.location.pathname)[1];
-            var url = common.getUrl('location-api', { locations: location_id });
-            var width = $(document).width();
-            $.getJSON(url, function(data) {
-                var loc = new location.Location(data.locations[0]),
-                    // need to build the template first so leaflet can find the map
-                    template = Handlebars.compile(html);
+            var location_id = /(\d+)/.exec(window.location.pathname)[1],
+                url = common.getUrl('location-api', { locations: location_id }),
+                width = $(document).width(),
+                $map = $('#location-map'),
+                latLng = new L.LatLng($map.data('lat'), $map.data('lng')),
+                map = new L.Map('location-map', { center: latLng, zoom: 13, dragging: false }),
+                $star = $('.favs-toggle');
 
-                $('.container > .row').append(template(loc.data));
-                $('.single-location-map').show();
-                if (width >= common.breakpoints.desktop) {
-                    $('.favs-toggle').show();
-                }
-
-                var latLng = loc.getLatLng(), 
-                    map = new L.Map('location-map', { center: latLng, zoom: 13, dragging: false });
+            if (width >= common.breakpoints.desktop) {
+                $('.favs-toggle').show();
+            }
                 
-                L.tileLayer.provider('Acetate.all').addTo(map);             // basemap 
+            L.tileLayer.provider('Acetate.all').addTo(map);             // basemap 
+            map.panTo(latLng);
+
+            if (favorites.isStarred(location_id)) {
+                favorites.toggle($star);
+            }
+
+            $('.single-share').show().on('click', function(e) {
+                $('#share-modal').trigger('init-modal', {                                           
+                    // the url is passed in to the sharing urls, so it must be absolute             
+                    url: common.getUrl('origin') + 
+                        common.getUrl('single-location', { location: location_id }), 
+                    title: 'Check out this early learning program'                                  
+                });
+            });
+
+            $.getJSON(url, function(data) {
+                var loc = new location.Location(data.locations[0]);
+
                 loc.setMarker({ popup: false });
                 loc.getMarker().addTo(map);
-                map.panTo(latLng);
 
-                var $star = $('.favs-toggle');
-                if (favorites.isStarred(location_id)) {
-                    favorites.toggle($star);
-                }
                 $star.on('click', function(e) {
                     favorites.toggle($star);
                     loc.setMarker();
-                });
-
-                $('.single-share').show().on('click', function(e) {
-                    $('#share-modal').trigger('init-modal', {                                           
-                        // the url is passed in to the sharing urls, so it must be absolute             
-                        url: common.getUrl('origin') + 
-                            common.getUrl('single-location', { location: location_id }), 
-                        title: 'Check out this early learning program'                                  
-                    });
                 });
             });
         });
