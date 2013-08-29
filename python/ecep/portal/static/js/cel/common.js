8de7d683de211821642d621330b199e6dd928afe
CloudNiner <andrew.fink.11@gmail.com>
2013-08-29 15:08:21 -0400

    Add basic GA click tracking on search page

--- python/ecep/portal/static/js/cel/common.js
+++ python/ecep/portal/static/js/cel/common.js
@@ -390,6 +390,20 @@ function($, L, Response, Handlebars) {
 
         isTouchscreen: isTouchscreen,
 
+        gaTrackEvent: function($elt) {
+            var category = CEL.serverVars.language + '/' + $elt.data('ga-category'),
+                action = $elt.data('ga-action'),
+                opt_label = $elt.data('ga-label'),
+                opt_value = $elt.data('ga-value'),
+                opt_noninteraction = $elt.data('ga-noninteraction'),
+                data = ['_trackEvent', category, action];
+            // optional parameters
+            data.push(opt_label || undefined);
+            data.push(opt_value || undefined);
+            data.push(!!(opt_noninteraction));
+            _gaq.push(data);
+        },
+
         // Stolen from _.js v1.5.1
         // https://github.com/jashkenas/underscore/blob/dc5a3fa0133b7000c36ba76a413139c63f646789/underscore.js
         // See project root for license
