b7288af2685fe705ec7a4f2e223140dc5f731ad1
Scott Robbin <scott@robbin.co>
2014-07-08 11:13:12 -0500

    Add curriculum field to admin.

--- python/ecep/portal/translation.py
+++ python/ecep/portal/translation.py
@@ -2,6 +2,6 @@ from modeltranslation.translator import translator, TranslationOptions
 from portal.models import Location
 
 class LocationTranslationOptions(TranslationOptions):
-    fields = ('q_stmt', 'open_house',)
+    fields = ('q_stmt', 'open_house', 'curriculum',)
 
 translator.register(Location, LocationTranslationOptions)
