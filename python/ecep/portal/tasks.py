cba01f4de9b7c87cb1750f772146ced232758eec
Frank Wiles <frank@revsys.com>
2015-05-13 15:44:09 -0500

    PEP8

--- python/ecep/portal/tasks.py
+++ python/ecep/portal/tasks.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.core import mail
-from django.template import Context
-from django.template.loader import get_template
+from django.template.loader import render_to_string
 from models import Location
 from celery import task
 import logging
@@ -9,34 +8,59 @@ import logging
 connection = mail.get_connection()
 logger = logging.getLogger(__name__)
 
-@task()
+
+@task
 def send_emails(inquirer_info, location_ids):
-  locations = Location.objects.filter(pk__in=location_ids)
-  messages = []
+    locations = Location.objects.filter(pk__in=location_ids)
+    messages = []
 
-  # Email the inquirer
-  i_template = get_template('email/inquirer.html').render(Context({
-      'info': inquirer_info,
-      'locations': locations,
-    }))
-  i_subject = "You contacted %d Chicago Early Learning locations" % len(locations)
-  i_message = mail.EmailMessage(i_subject, i_template, settings.DEFAULT_FROM_EMAIL, [inquirer_info['email']])
-  i_message.content_subtype = "html"
-  messages.append(i_message)
-
-  # Group by location's email
-  location_emails = {}
-  for l in locations:
-    location_emails.setdefault(l.email,[]).append(l)
-  for e, l in location_emails.iteritems():
-    l_template = get_template('email/location.html').render(Context({
+    # Email the inquirer
+    i_template = render_to_string('email/inquirer.html', {
         'info': inquirer_info,
-        'locations': l,
-      }))
-    l_subject = "Chicago Early Learning inquiry from %s %s" % (inquirer_info['first_name'], inquirer_info['last_name'])
-    l_message = mail.EmailMessage(l_subject, l_template, inquirer_info['email'], [e], headers={'Reply-To': inquirer_info['first_name']})
-    l_message.content_subtype = "html"
-    messages.append(l_message)
+        'locations': locations,
+    })
 
-  # Send all emails
-  connection.send_messages(messages)
+    i_subject = "You contacted {} Chicago Early Learning locations".format(
+        len(locations)
+    )
+
+    i_message = mail.EmailMessage(
+        i_subject,
+        i_template,
+        settings.DEFAULT_FROM_EMAIL,
+        [inquirer_info['email']],
+    )
+
+    i_message.content_subtype = "html"
+    messages.append(i_message)
+
+    # Group by location's email
+    location_emails = {}
+    for l in locations:
+        location_emails.setdefault(l.email, []).append(l)
+
+    for e, l in location_emails.iteritems():
+        l_template = render_to_string('email/location.html', {
+            'info': inquirer_info,
+            'locations': l,
+        })
+
+        l_subject = "Chicago Early Learning inquiry from {} {}".format(
+            inquirer_info['first_name'],
+            inquirer_info['last_name'],
+        )
+
+        l_message = mail.EmailMessage(
+            l_subject,
+            l_template,
+            inquirer_info['email'],
+            [e],
+            headers={
+                'Reply-To': inquirer_info['first_name']
+            },
+        )
+        l_message.content_subtype = "html"
+        messages.append(l_message)
+
+    # Send all emails
+    connection.send_messages(messages)
