bcf4258247781d69149d78154b435390d0cf53cb
Andrew Briggs <theandrewbriggs@gmail.com>
2016-04-07 13:50:00 -0500

    Reconfigure retina detection

--- python/ecep/portal/static/js/cel/pages/browse.js
+++ python/ecep/portal/static/js/cel/pages/browse.js
@@ -485,11 +485,18 @@ define(['jquery', 'Leaflet', 'Handlebars', 'text!templates/neighborhoodList.html
                     historyState = History.getState().data,
                     zoom = defaultZoom,
                     qs = qs2Obj(),
-                    label = qs.label;
+                    label = qs.label,
+                    mapboxURL = '';
+
+                if (common.isRetinaDisplay()) {
+                    mapboxURL = 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}@2x.png?access_token='
+                } else {
+                    mapboxURL = 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token='
+                }
 
                 var accessToken = 'pk.eyJ1IjoidGhlYW5kcmV3YnJpZ2dzIiwiYSI6ImNpaHh2Z2hpcDAzZnd0bG0xeDNqYXdiOGkifQ.jV7_LuEh4KX2r5RudiQdIg';
-                var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=' + accessToken,
-                    {attribution: 'Imagery from <a href="http://mapbox.com/about/maps/">MapBox</a>', detectRetina: true});
+                var mapboxTiles = L.tileLayer(mapboxURL + accessToken,
+                    {attribution: 'Imagery from <a href="http://mapbox.com/about/maps/">MapBox</a>'});
 
                 map = new L.map('map').setView(state.point, zoom);   // Initialize Leaflet map
                 map.addLayer(mapboxTiles);
