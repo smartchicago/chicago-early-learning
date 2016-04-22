12f4b60d2e38d5cc1082e45f607f69eeede81fb0
Andrew Briggs <theandrewbriggs@gmail.com>
2016-04-22 12:50:15 -0500

    Pull trailing backslash

--- python/ecep/portal/static/js/cel/common.js
+++ python/ecep/portal/static/js/cel/common.js
@@ -176,7 +176,7 @@ function($, L, Response, Handlebars) {
 
         // Fetch Location names 
         var location_list;
-        $.get('/api/location/json/', function (data) {
+        $.get('/api/location/json', function (data) {
             location_list = data;
             console.log(location_list);
         }, 'json');
