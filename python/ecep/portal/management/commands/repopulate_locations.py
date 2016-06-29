36cbc2093a827b6a0295d81a281ffc90343f3b63
Andrew Briggs <theandrewbriggs@gmail.com>
2016-06-29 17:39:28 -0500

    Base for repopulation script

---
+++ python/ecep/portal/management/commands/repopulate_locations.py
@@ -0,0 +1,17 @@
+import random
+import csv
+
+from django.core.management.base import BaseCommand, CommandError
+
+from portal.models import Location
+
+
+class Command(BaseCommand):
+    """
+    """
+
+    def handle(self, *args, **options):
+        """
+        """
+
+        pass
