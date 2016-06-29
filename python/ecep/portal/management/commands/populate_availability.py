0d74d6a12ab7fbdd89a8a21ad10abb6b97932998
Andrew Briggs <theandrewbriggs@gmail.com>
2016-06-29 17:18:53 -0500

    Add base for availability population

---
+++ python/ecep/portal/management/commands/populate_availability.py
@@ -0,0 +1,26 @@
+import random
+import csv
+
+from django.core.management.base import BaseCommand, CommandError
+
+from portal.models import Location
+
+
+class Command(BaseCommand):
+    """
+    """
+
+    def handle(self, *args, **options):
+        """
+        """
+
+        with open('availability.csv', 'rb') as availability_file:
+            reader = csv.DictReader(availability_file)
+            
+            for row in reader:
+
+                try:
+                    key = row['Key']
+                    l = Location.objects.get(ecm_key=key)
+                    print l.site_name
+
