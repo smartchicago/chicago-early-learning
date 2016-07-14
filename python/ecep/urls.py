1f1c5be2b6ff07c7681bb38af35c545b522e90c7
Andrew Briggs <theandrewbriggs@gmail.com>
2016-07-14 11:44:06 -0500

    Redirect favicon URL

--- python/ecep/urls.py
+++ python/ecep/urls.py
@@ -31,7 +31,7 @@ urlpatterns = patterns(
     url(r'^city-resources$', portal_views.CityResources.as_view(), name='city-resources'),
     url(r'^how-to-apply$', portal_views.HowToApply.as_view(), name='how-to-apply'),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type="text/plain")),
-    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico')),
     
     # browse page
     url(r'^search/$', 'portal.views.browse', name='browse'),
