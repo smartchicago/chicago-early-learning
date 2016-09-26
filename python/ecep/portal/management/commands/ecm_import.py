5eb9449e5ea93b39ac9b3a73c51f980a6d74c77f
Andrew Briggs <theandrewbriggs@gmail.com>
2016-09-26 16:20:45 -0500

    Add save to ecm import script

--- python/ecep/portal/management/commands/ecm_import.py
+++ python/ecep/portal/management/commands/ecm_import.py
@@ -23,68 +23,78 @@ class Command(BaseCommand):
 
             for row in reader:
 
-                key = row['ECMKey']
-                l = Location.objects.get(ecm_key=key)
+                try:
+                    key = row['ECMKey']
+                    l = Location.objects.get(ecm_key=key)
 
-                print ""
-                print key
-                print l.site_name
+                    print ""
+                    print key
+                    print l.site_name
 
-                if l.address != row['LocAddress'] and row['LocAddress'] is not None:
+                    if l.address != row['LocAddress'] and row['LocAddress'] is not None:
 
-                    location = geocode(row['LocAddress'], row['LocCity'], row['LocState'], row['LocZip'])
-                    l.address = row['LocAddress']
-                    print "Updated address to {}".format(row['LocAddress'])
-                    l.geom = 'POINT({} {})'.format(location.longitude, location.latitude)
-                    print 'Updated geometry!'
+                        location = geocode(row['LocAddress'], row['LocCity'], row['LocState'], row['LocZip'])
+                        l.address = row['LocAddress']
+                        print "Updated address to {}".format(row['LocAddress'])
+                        l.geom = 'POINT({} {})'.format(location.longitude, location.latitude)
+                        print 'Updated geometry!'
 
-                l.ages = smart_swap(l.ages, row['Ages_Served'], l.verbose_name('ages'))
+                    l.ages = smart_swap(l.ages, row['Ages_Served'], l.verbose_name('ages'))
 
-                l.q_rating = smart_swap(l.q_rating, row['Quality_Rating'], l.verbose_name('q_rating'))
+                    l.q_rating = smart_swap(l.q_rating, row['Quality_Rating'], l.verbose_name('q_rating'))
 
-                l.is_part_day = smart_swap(l.is_part_day, parse_null_boolean(row['Part_Day_Y_N']), l.verbose_name('is_part_day'))
+                    l.is_part_day = smart_swap(l.is_part_day, parse_null_boolean(row['Part_Day_Y_N']), l.verbose_name('is_part_day'))
 
-                l.prg_hours = smart_swap(l.prg_hours, row['Operating_Hours'], l.verbose_name('prg_hours'))
+                    l.prg_hours = smart_swap(l.prg_hours, row['Operating_Hours'], l.verbose_name('prg_hours'))
 
-                l.city = smart_swap(l.city, row['LocCity'], l.verbose_name('city'))
+                    l.city = smart_swap(l.city, row['LocCity'], l.verbose_name('city'))
 
-                l.zip = smart_swap(l.zip, row['LocZip'], l.verbose_name('zip'))
+                    l.zip = smart_swap(l.zip, row['LocZip'], l.verbose_name('zip'))
 
-                l.state = smart_swap(l.state, row['LocState'], l.verbose_name('state'))
+                    l.state = smart_swap(l.state, row['LocState'], l.verbose_name('state'))
 
-                l.is_age_lt_3 = smart_swap(l.is_age_lt_3, parse_null_boolean(row['Ages_Zero_Three_Y_N']), l.verbose_name('is_age_lt_3'))
+                    l.is_age_lt_3 = smart_swap(l.is_age_lt_3, parse_null_boolean(row['Ages_Zero_Three_Y_N']), l.verbose_name('is_age_lt_3'))
 
-                l.is_age_gt_3 = smart_swap(l.is_age_gt_3, parse_null_boolean(row['Ages_Three_Five_Y_N']), l.verbose_name('is_age_gt_3'))
+                    l.is_age_gt_3 = smart_swap(l.is_age_gt_3, parse_null_boolean(row['Ages_Three_Five_Y_N']), l.verbose_name('is_age_gt_3'))
 
-                l.is_community_based = smart_swap(l.is_community_based, parse_null_boolean(row['Community_Based_Y_N']), l.verbose_name('is_community_based'))
+                    l.is_community_based = smart_swap(l.is_community_based, parse_null_boolean(row['Community_Based_Y_N']), l.verbose_name('is_community_based'))
 
-                l.is_school_year = smart_swap(l.is_school_year, parse_null_boolean(row['School_Year_Y_N']), l.verbose_name('is_school_year'))
+                    l.is_school_year = smart_swap(l.is_school_year, parse_null_boolean(row['School_Year_Y_N']), l.verbose_name('is_school_year'))
 
-                l.accred = smart_swap(l.accred, row['Accreditation'], l.verbose_name('accred'))
+                    l.accred = smart_swap(l.accred, row['Accreditation'], l.verbose_name('accred'))
 
-                l.language_1 = smart_swap(l.language_1, row['Languages_other_than_English'], l.verbose_name('language_1'))
-                l.language_2 = smart_swap(l.language_2, row['Lang2'], l.verbose_name('language_2'))
-                l.language_3 = smart_swap(l.language_3, row['Lang3'], l.verbose_name('language_3'))
+                    l.language_1 = smart_swap(l.language_1, row['Languages_other_than_English'], l.verbose_name('language_1'))
+                    l.language_2 = smart_swap(l.language_2, row['Lang2'], l.verbose_name('language_2'))
+                    l.language_3 = smart_swap(l.language_3, row['Lang3'], l.verbose_name('language_3'))
 
-                l.phone = smart_swap(l.phone, row['Phone_Number'], l.verbose_name('phone'))
+                    l.phone = smart_swap(l.phone, row['Phone_Number'], l.verbose_name('phone'))
 
-                l.accept_ccap = smart_swap(l.accept_ccap, parse_null_boolean(row['Accepts_CCAP_Y_N']), l.verbose_name('accept_ccap'))
+                    l.accept_ccap = smart_swap(l.accept_ccap, parse_null_boolean(row['Accepts_CCAP_Y_N']), l.verbose_name('accept_ccap'))
 
-                l.address = smart_swap(l.address, row['LocAddress'], l.verbose_name('address'))
+                    l.address = smart_swap(l.address, row['LocAddress'], l.verbose_name('address'))
 
-                l.is_hs = smart_swap(l.is_hs, parse_null_boolean(row['Head_Start_Y_N']), l.verbose_name('is_hs'))
+                    l.is_hs = smart_swap(l.is_hs, parse_null_boolean(row['Head_Start_Y_N']), l.verbose_name('is_hs'))
 
-                l.is_full_day = smart_swap(l.is_full_day, parse_null_boolean(row['Full_Day_Y_N']), l.verbose_name('is_full_day'))
+                    l.is_full_day = smart_swap(l.is_full_day, parse_null_boolean(row['Full_Day_Y_N']), l.verbose_name('is_full_day'))
 
-                l.is_full_year = smart_swap(l.is_full_year, parse_null_boolean(row['Full_Year_Y_N']), l.verbose_name('is_full_year'))
+                    l.is_full_year = smart_swap(l.is_full_year, parse_null_boolean(row['Full_Year_Y_N']), l.verbose_name('is_full_year'))
 
-                l.url = smart_swap(l.url, row['URL'], l.verbose_name('url'))
+                    l.url = smart_swap(l.url, row['URL'], l.verbose_name('url'))
 
-                l.is_home_visiting = smart_swap(l.is_home_visiting, parse_null_boolean(row['Home_Visiting_Y_N']), l.verbose_name('is_home_visiting'))
+                    l.is_home_visiting = smart_swap(l.is_home_visiting, parse_null_boolean(row['Home_Visiting_Y_N']), l.verbose_name('is_home_visiting'))
 
-                l.is_cps_based = smart_swap(l.is_cps_based, parse_null_boolean(row['School_Based_Y_N']), l.verbose_name('is_cps_based'))
+                    l.is_cps_based = smart_swap(l.is_cps_based, parse_null_boolean(row['School_Based_Y_N']), l.verbose_name('is_cps_based'))
 
-                l.save()
+                    l.save()
+
+                except:
+
+                    print ''
+                    print '*********'
+                    print "Whoops! Something went wrong with "
+                    print row['ECMKey']
+                    print '*********'
+                    print ''
 
 
 
