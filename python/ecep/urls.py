c8f0702cbd6369b0b4c09b06a19529607821bf04
Andrew Briggs <theandrewbriggs@gmail.com>
2017-03-28 17:55:56 -0500

    Remove test url

--- python/ecep/urls.py
+++ python/ecep/urls.py
@@ -24,7 +24,6 @@ urlpatterns = patterns(
     '',
     # Index page is in the 'portal' app
     url(r'^$', portal_views.Index.as_view(), name='index'),
-    url(r'^test$', portal_views.Test.as_view(), name='test'),
     url(r'^about$', portal_views.About.as_view(), name='about'),
     url(r'^updates$', portal_views.Updates.as_view(), name='updates'),
     url(r'^families$', portal_views.Families.as_view(), name='families'),
