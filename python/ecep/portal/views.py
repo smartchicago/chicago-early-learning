63c111457f96913bf9d7d4c53c8148d4782c3a35
Andrew Briggs <theandrewbriggs@gmail.com>
2017-05-09 20:16:45 -0500

    Replace ECM in data payload with COPA ID

--- python/ecep/portal/views.py
+++ python/ecep/portal/views.py
@@ -610,12 +610,12 @@ def starred_location_api(request, location_ids=None):
                                      kwargs = {'location_id': location.id})
 
         # Is 
-        e = {}
-        ecm_warning_display = ugettext('Contact this site directly to apply.')
-        e['warning'] = ecm_warning_display
-        e['key'] = location.ecm_key
-        e['value'] = (location.ecm_key == 0)
-        l['ecm'] = e
+        copa = {}
+        copa_warning_display = ugettext('Contact this site directly to apply.')
+        copa['warning'] = copa_warning_display
+        copa['key'] = location.copa_key
+        copa['value'] = (location.copa_key == 0)
+        l['copa'] = copa
 
 
         # Description
