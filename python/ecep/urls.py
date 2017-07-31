d88bd1ff3f296f6926cf2ee91ee8d6878140ebf3
Andrew Briggs <theandrewbriggs@gmail.com>
2017-07-30 22:07:24 -0500

    A BUNCH of changes

--- python/ecep/urls.py
+++ python/ecep/urls.py
@@ -40,7 +40,7 @@ urlpatterns = patterns(
 
     # Blog
     url(r'^blog/$', Blog.as_view(), name="blog"),
-    url(r'^blog/hughes-library$', Blog.as_view(), name="blog"),
+    url(r'^blog/hughes-library$', Blog.as_view(), name="blog-hughes"),
 
     # browse page
     url(r'^search/$', 'portal.views.browse', name='browse'),
